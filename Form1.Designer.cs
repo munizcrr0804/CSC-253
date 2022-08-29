83a3a1caf369e461782ddac5ee0493483ca4f5a7
munizcrr0804 <55550903+munizcrr0804@users.noreply.github.com>
2022-08-29 09:05:13 -0400

    M1HW1

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace KineticEnergy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.massTextBox = new System.Windows.Forms.TextBox();
+            this.velocityTextBox = new System.Windows.Forms.TextBox();
+            this.btnCalcKE = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblKineticEnergy = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(347, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kinetic Energy Application";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(319, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mass in kg (m) :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(319, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Velocity in mps (v) : ";
+            // 
+            // massTextBox
+            // 
+            this.massTextBox.Location = new System.Drawing.Point(430, 112);
+            this.massTextBox.Name = "massTextBox";
+            this.massTextBox.Size = new System.Drawing.Size(100, 20);
+            this.massTextBox.TabIndex = 3;
+            // 
+            // velocityTextBox
+            // 
+            this.velocityTextBox.Location = new System.Drawing.Point(430, 155);
+            this.velocityTextBox.Name = "velocityTextBox";
+            this.velocityTextBox.Size = new System.Drawing.Size(100, 20);
+            this.velocityTextBox.TabIndex = 4;
+            // 
+            // btnCalcKE
+            // 
+            this.btnCalcKE.Location = new System.Drawing.Point(333, 211);
+            this.btnCalcKE.Name = "btnCalcKE";
+            this.btnCalcKE.Size = new System.Drawing.Size(75, 60);
+            this.btnCalcKE.TabIndex = 5;
+            this.btnCalcKE.Text = "Calculate Kinetic Energy";
+            this.btnCalcKE.UseVisualStyleBackColor = true;
+            this.btnCalcKE.Click += new System.EventHandler(this.btnCalcKE_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(430, 211);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(333, 292);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Kinetic Energy (J) :";
+            // 
+            // lblKineticEnergy
+            // 
+            this.lblKineticEnergy.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblKineticEnergy.Location = new System.Drawing.Point(442, 293);
+            this.lblKineticEnergy.Name = "lblKineticEnergy";
+            this.lblKineticEnergy.Size = new System.Drawing.Size(100, 23);
+            this.lblKineticEnergy.TabIndex = 8;
+            this.lblKineticEnergy.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblKineticEnergy);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalcKE);
+            this.Controls.Add(this.velocityTextBox);
+            this.Controls.Add(this.massTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Kinetic Energy";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox massTextBox;
+        private System.Windows.Forms.TextBox velocityTextBox;
+        private System.Windows.Forms.Button btnCalcKE;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblKineticEnergy;
+    }
+}
+

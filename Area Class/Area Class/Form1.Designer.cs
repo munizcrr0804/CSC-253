bd0600d33d3cfed0841bc1d252a4eb3f439ce571
munizcrr0804 <55550903+munizcrr0804@users.noreply.github.com>
2022-12-07 08:05:58 -0500

    Add files via upload

---
+++ Area Class/Area Class/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿
+namespace Area_Class
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRadiusCircle = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAreaCircle = new System.Windows.Forms.TextBox();
+            this.btn_AreaCircle = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.btn_AreaRectangle = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtAreaRectangle = new System.Windows.Forms.TextBox();
+            this.txtRadiusCylinder = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtAreaCylinder = new System.Windows.Forms.TextBox();
+            this.btn_AreaCylinder = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Circle";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter the circle\'s radius";
+            // 
+            // txtRadiusCircle
+            // 
+            this.txtRadiusCircle.Location = new System.Drawing.Point(257, 98);
+            this.txtRadiusCircle.Name = "txtRadiusCircle";
+            this.txtRadiusCircle.Size = new System.Drawing.Size(100, 20);
+            this.txtRadiusCircle.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(195, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Area";
+            // 
+            // txtAreaCircle
+            // 
+            this.txtAreaCircle.Location = new System.Drawing.Point(257, 148);
+            this.txtAreaCircle.Name = "txtAreaCircle";
+            this.txtAreaCircle.Size = new System.Drawing.Size(100, 20);
+            this.txtAreaCircle.TabIndex = 4;
+            // 
+            // btn_AreaCircle
+            // 
+            this.btn_AreaCircle.Location = new System.Drawing.Point(208, 187);
+            this.btn_AreaCircle.Name = "btn_AreaCircle";
+            this.btn_AreaCircle.Size = new System.Drawing.Size(149, 23);
+            this.btn_AreaCircle.TabIndex = 5;
+            this.btn_AreaCircle.Text = "Get the Circle\'s Area";
+            this.btn_AreaCircle.UseVisualStyleBackColor = true;
+            this.btn_AreaCircle.Click += new System.EventHandler(this.btn_AreaCircle_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Rectangle";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(109, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Enter the width";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(109, 289);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Enter the length";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(258, 246);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 9;
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(257, 282);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 10;
+            // 
+            // btn_AreaRectangle
+            // 
+            this.btn_AreaRectangle.Location = new System.Drawing.Point(209, 349);
+            this.btn_AreaRectangle.Name = "btn_AreaRectangle";
+            this.btn_AreaRectangle.Size = new System.Drawing.Size(159, 23);
+            this.btn_AreaRectangle.TabIndex = 11;
+            this.btn_AreaRectangle.Text = "Get the Rectangle\'s Area";
+            this.btn_AreaRectangle.UseVisualStyleBackColor = true;
+            this.btn_AreaRectangle.Click += new System.EventHandler(this.btn_AreaRectangle_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(99, 375);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Cylinder";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(108, 408);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(81, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Enter the radius";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(108, 440);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(82, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Enter the height";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(170, 325);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(29, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Area";
+            // 
+            // txtAreaRectangle
+            // 
+            this.txtAreaRectangle.Location = new System.Drawing.Point(257, 318);
+            this.txtAreaRectangle.Name = "txtAreaRectangle";
+            this.txtAreaRectangle.Size = new System.Drawing.Size(100, 20);
+            this.txtAreaRectangle.TabIndex = 16;
+            // 
+            // txtRadiusCylinder
+            // 
+            this.txtRadiusCylinder.Location = new System.Drawing.Point(226, 408);
+            this.txtRadiusCylinder.Name = "txtRadiusCylinder";
+            this.txtRadiusCylinder.Size = new System.Drawing.Size(100, 20);
+            this.txtRadiusCylinder.TabIndex = 17;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(226, 440);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 18;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(158, 469);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(29, 13);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "Area";
+            // 
+            // txtAreaCylinder
+            // 
+            this.txtAreaCylinder.Location = new System.Drawing.Point(226, 469);
+            this.txtAreaCylinder.Name = "txtAreaCylinder";
+            this.txtAreaCylinder.Size = new System.Drawing.Size(100, 20);
+            this.txtAreaCylinder.TabIndex = 20;
+            // 
+            // btn_AreaCylinder
+            // 
+            this.btn_AreaCylinder.Location = new System.Drawing.Point(209, 512);
+            this.btn_AreaCylinder.Name = "btn_AreaCylinder";
+            this.btn_AreaCylinder.Size = new System.Drawing.Size(159, 23);
+            this.btn_AreaCylinder.TabIndex = 21;
+            this.btn_AreaCylinder.Text = "Get the Rectangle\'s Area";
+            this.btn_AreaCylinder.UseVisualStyleBackColor = true;
+            this.btn_AreaCylinder.Click += new System.EventHandler(this.btn_AreaCylinder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 547);
+            this.Controls.Add(this.btn_AreaCylinder);
+            this.Controls.Add(this.txtAreaCylinder);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtRadiusCylinder);
+            this.Controls.Add(this.txtAreaRectangle);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btn_AreaRectangle);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_AreaCircle);
+            this.Controls.Add(this.txtAreaCircle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtRadiusCircle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRadiusCircle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAreaCircle;
+        private System.Windows.Forms.Button btn_AreaCircle;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Button btn_AreaRectangle;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtAreaRectangle;
+        private System.Windows.Forms.TextBox txtRadiusCylinder;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtAreaCylinder;
+        private System.Windows.Forms.Button btn_AreaCylinder;
+    }
+}
+
---
+++ Area Class/Area Class/Form1.cs
@@ -0,0 +1,89 @@
+﻿/**
+* 8/25/2022
+* CSC 253
+* Ramon Muniz Crespo
+* Area class
+*/
+
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Area_Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static double area(double radius)
+        {
+            return (Math.PI * radius * radius);
+        }
+
+        private static double area(int width, int length)
+        {
+            return (width * length);
+        }
+
+        private static double area(double radius, double height)
+        {
+            return (Math.PI * radius * radius * height);
+        }
+
+
+        private void btn_AreaCircle_Click(object sender, EventArgs e)
+        {
+            double radius;
+
+            radius = Convert.ToInt32(txtRadiusCircle.Text);
+
+            txtAreaCircle.Text = Math.Round(area(radius), 2).ToString();
+
+        }
+
+        private void btn_AreaRectangle_Click(object sender, EventArgs e)
+        {
+            int length, width;
+
+            length = Convert.ToInt32(txtWidth.Text);
+
+            width = Convert.ToInt32(txtLength.Text);
+
+            txtAreaRectangle.Text = Math.Round(area(length, width), 2).ToString();
+
+
+        }
+
+        private void btn_AreaCylinder_Click(object sender, EventArgs e)
+        {
+            double radius, height;
+
+            radius = Convert.ToDouble(txtRadiusCylinder.Text);
+
+            height = Convert.ToDouble(txtHeight.Text);
+
+            txtAreaCylinder.Text = Math.Round(area(radius, height), 2).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtAreaCircle.Enabled = false;
+            txtAreaRectangle.Enabled = false;
+            txtAreaCylinder.Enabled = false;
+        }
+    }
+}
